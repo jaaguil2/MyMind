cc30d10909e26053210a7545ba26ca857735b0ec
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-18 11:23:16 -0400

    added base http calling

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
+
+  
 }
