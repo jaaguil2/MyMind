bb48b4e146bfd1d0fcafee41e729ab23caaa1fc1
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-21 16:50:18 -0400

    more updating

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 // Interface
 import { UserSignIn } from './interface/userSignIn';
@@ -12,19 +12,24 @@ import { User } from './interface/user';
 })
 export class UserService {
 
-  
+  httpOptions = {
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json' ,
+      'Access-Control-Allow-Origin': '*'
+    })
+  };
 
   private apiUrl = 'https://mymind-be.herokuapp.com/api/'
 
   constructor(private http: HttpClient) { }
 
   signIn(signIn: UserSignIn) {
-    return this.http.post(this.apiUrl+"signin", signIn)
+    return this.http.post(this.apiUrl + "signin", signIn, this.httpOptions)
       .pipe(catchError(this.handleError))
   }
 
   signUp(user: User) {
-    return this.http.post(this.apiUrl+'signup', user)
+    return this.http.post(this.apiUrl+'signup', user, this.httpOptions)
       .pipe(catchError(this.handleError))
   }
 
