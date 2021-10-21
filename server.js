d39d73180e36df340fbc6399e7b01fd153b594af
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2021-10-21 17:06:27 -0400

    installed cors to server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
+app.use(cors())
 app.use(express.static('./dist/MyMind'));
 
 app.get('/*', (req, res) =>
